6cc6a013fe18c657a453fd0ff42947498d5f5811
Chuck Lu <chuck.lu@qq.com>
2020-07-31 19:28:19 +0800

    remove useless control

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,23 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.tick = new System.Windows.Forms.Timer(this.components);
             this.button1 = new System.Windows.Forms.Button();
-            this.TP = new System.Windows.Forms.ToolTip(this.components);
             this.lbmsg = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(118, 8);
+            this.button2.Location = new System.Drawing.Point(177, 13);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(151, 34);
+            this.button2.Size = new System.Drawing.Size(226, 57);
             this.button2.TabIndex = 1;
             this.button2.Text = "整活！";
             this.button2.UseVisualStyleBackColor = true;
@@ -53,61 +51,58 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 94);
+            this.label1.Location = new System.Drawing.Point(18, 157);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(245, 12);
+            this.label1.Size = new System.Drawing.Size(350, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "思路采自NGA@w_what，窗口程序：b站@千岛秋";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(25, 16);
+            this.textBox1.Location = new System.Drawing.Point(38, 27);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(28, 21);
+            this.textBox1.Size = new System.Drawing.Size(40, 26);
             this.textBox1.TabIndex = 3;
             this.textBox1.Text = "5";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(59, 19);
+            this.label2.Location = new System.Drawing.Point(88, 32);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.Size = new System.Drawing.Size(73, 20);
             this.label2.TabIndex = 4;
             this.label2.Text = "秒后重连";
             // 
-            // tick
-            // 
-            this.tick.Interval = 1000;
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(25, 57);
+            this.button1.Location = new System.Drawing.Point(38, 95);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(80, 34);
+            this.button1.Size = new System.Drawing.Size(120, 57);
             this.button1.TabIndex = 5;
             this.button1.Text = "网络恢复";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // TP
-            // 
-            this.TP.IsBalloon = true;
-            // 
             // lbmsg
             // 
             this.lbmsg.AutoSize = true;
-            this.lbmsg.Location = new System.Drawing.Point(154, 69);
+            this.lbmsg.Location = new System.Drawing.Point(231, 115);
+            this.lbmsg.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbmsg.Name = "lbmsg";
-            this.lbmsg.Size = new System.Drawing.Size(0, 12);
+            this.lbmsg.Size = new System.Drawing.Size(0, 20);
             this.lbmsg.TabIndex = 6;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(272, 115);
+            this.ClientSize = new System.Drawing.Size(408, 192);
             this.Controls.Add(this.lbmsg);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
@@ -115,8 +110,9 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(350, 200);
+            this.MaximumSize = new System.Drawing.Size(514, 296);
             this.Name = "Form1";
             this.Text = "酒馆战棋整活插件 2.0";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -130,9 +126,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Timer tick;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ToolTip TP;
         private System.Windows.Forms.Label lbmsg;
     }
 }
